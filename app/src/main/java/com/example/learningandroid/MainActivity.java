5c23264d9e6bb6b63d82595b5d0893feedcd30e7
Nguyễn Xuân Hiếu <77017287+BeMyFriendPlz@users.noreply.github.com>
2024-01-19 21:24:44 +0700

    commit 2

--- app/src/main/java/com/example/learningandroid/MainActivity.java
+++ app/src/main/java/com/example/learningandroid/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+public class MainActivity extends AppCompatActivity {
+    private Spinner sp1, sp2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.widgets_excer);
+        sp2 = findViewById(R.id.s2);
+        String lst[] = {"PTIT", "HUST", "FPT", "NEU"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.item, lst);
+        sp2.setAdapter(adapter);
+
+        sp1 = findViewById(R.id.s1);
+        String list[] = getResources().getStringArray(R.array.countries);
+        ArrayAdapter<String> ad = new ArrayAdapter<>(this, R.layout.item, list);
+        sp1.setAdapter(ad);
     }
 }
